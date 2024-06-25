93950bee0556a6404fc4a7e3ee8dd239a9ec7db5
kiruthiga sri D <95760018+kirthikasri@users.noreply.github.com>
2024-06-25 20:59:39 +0530

    Update scrape_and_publish.py

--- scrape_and_publish.py
+++ scrape_and_publish.py
@@ -1,37 +1,45 @@
+#importing the libraries
 import requests
 from bs4 import BeautifulSoup
 import json
 from google.cloud import pubsub_v1
-import os
+import time
+import base64
+import pandas as pd
+import pyarrow
+
+
+#intializingpub/sub client
+project_id=''
+topic_id=''
+publisher = pubsub_v1.publisherClient()
+topic_path = publisher.topic_path(project_id,topic_id)
+
+#intialize bigquery client
+client = bigquery.client()
+
 
 # Scrape data from the  website
-def scrape_converse():
-    url = ''
+def fetch_covid_data():
+    url = "https://www.worldometers.info/coronavirus/'
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    list = []
-    for item in soup.select('.product-card'):
-        shoe = {
-            'region': item.select_one('.region').get_text(strip=True),
-            'count': item.select_one('.coubt').get_text(strip=True),
-        }
-        list.append(shoe)
-    
-    return list
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data={}
+    data['total_cases']=soup.find("div",{"class":"maincounter-number"}).get_txt(strip=True)
+    data['deaths']=soup.find_all("div",{"class":"maincounter-number"})[1].get_txt(strip=True)
+    data['recovered']=soup.find_all("div",{"class":"maincounter-number"})[2].get_txt(strip=True)
+    return data
+
 
 # Publish messages to Pub/Sub
 def publish_to_pubsub(data):
-    project_id = os.getenv('')
-    topic_id = os.getenv('')
-    
-    publisher = pubsub_v1.PublisherClient()
-    topic_path = publisher.topic_path(project_id, topic_id)
-    
-    for item in data:
-        publisher.publish(topic_path, json.dumps(item).encode('utf-8'))
-        print(f'Published message: {item}')
+    #coverting the data into json 
+    data_str = json.dumps(data)
+    #encode the data to bytes
+    data_bytes = data_str.encode('utf-8')
+    #publish the data to pubsub
+    future = publisher.publish(topic_path,data=data_bytes)
+    print(f"publish message ID:{future.result()}")
 
-if __name__ == '__main__':
-    scraped_data = scrape_converse()
-    publish_to_pubsub(scraped_data)
+
+
